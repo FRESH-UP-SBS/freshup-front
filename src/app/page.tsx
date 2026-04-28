15cf61550dbbacde4549e6ec3b4c871ebe23cc9a
elena <tool0923@naver.com>
2026-04-28 22:09:40 +0900

    로그인 페이지 UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,10 @@ import LoginButton from '@/components/LoginButton';
 export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      <h1 className="text-3xl font-bold mb-8">청소 관리 시스템</h1>
+      <div className="main-title-box mb-15">
+        <h1 className="text-3xl font-bold mb-8">청소를 시작해볼까요? 🔥</h1>
+      </div>
+
       <LoginButton />
     </main>
   );
--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -1,16 +1,27 @@
 'use client';
 
+import Image from 'next/image';
+
 export default function LoginButton() {
     const handleLogin = () => {
         window.location.href = process.env.NEXT_PUBLIC_KAKAO_LOGIN_URL!;
     };
 
     return (
-        <button
-            onClick={handleLogin}
-            className="bg-yellow-400 text-black px-6 py-3 rounded-lg font-bold hover:bg-yellow-500 transition"
-        >
-            카카오로 로그인
-        </button>
+        <div>
+            <button className="flex w-90 justify-center" onClick={handleLogin}>
+                <Image src="/images/kakao_login_large_wide.png" alt="Kakao Icon" width={300} height={0} style={{ height: 'auto', width: '90%' }} className="mr-2" />
+            </button>
+
+
+        </div>
+
+
+        // <button
+        //     onClick={handleLogin}
+        //     className="bg-400 text-black px-6 py-3 rounded-lg"
+        // >
+
+        // </button>
     );
 }
