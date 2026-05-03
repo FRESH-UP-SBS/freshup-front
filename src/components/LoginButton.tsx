d74e8fe2d26e9a679d517c6ea4893c90438d1712
elena <tool0923@naver.com>
2026-05-04 00:58:01 +0900

    feature: 로그인 기능 구현

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -1,19 +1,20 @@
 'use client';
 
 import Image from 'next/image';
+import { de } from 'react-day-picker/locale';
 
 export default function LoginButton() {
     const handleLogin = () => {
+        console.log('Redirecting to Kakao login URL:', process.env.NEXT_PUBLIC_KAKAO_LOGIN_URL);
         window.location.href = process.env.NEXT_PUBLIC_KAKAO_LOGIN_URL!;
+
     };
 
     return (
         <div>
-            <button className="flex w-90 justify-center" onClick={handleLogin}>
+            <button className="flex w-90 justify-center cursor-pointer" onClick={handleLogin}>
                 <Image src="/images/kakao_login_large_wide.png" alt="Kakao Icon" width={300} height={0} style={{ height: 'auto', width: '90%' }} className="mr-2" />
             </button>
-
-
         </div>
 
 
